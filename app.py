91abd7c25950e5a3da4f758fd75e3234cb7bbeb3
Blaine Stockman <blaine-stockman@uiowa.edu>
2025-11-03 20:52:07 -0600

    Added static, templates, and updated app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 # module 10 - Flask Application
 # Blaine Stockman 11/3/2025
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/") # root route or home route
-def hello():
-    return "Hello World!!"
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+     return"This is Blaine's about page."
 
 if __name__ == "__main__":
       app.run(host="0.0.0.0", port=5002, debug=True)
